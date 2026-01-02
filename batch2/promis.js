305b6303c4d85de9d8006310801af005cbec6cf3
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-02 08:50:06 +0530

    s

--- batch2/promis.js
+++ batch2/promis.js
@@ -1,24 +1,42 @@
-// callback funtion
-let Mycallbackfunction = () =>{
-    console.log("This is my callback function");
+// // callback funtion
+// let Mycallbackfunction = () =>{
+//     console.log("This is my callback function");
 
-};
-let MyHigherOrderFunction = (callback) =>{
-    console.log("This is my higher order function");
-    callback();
-};
-MyHigherOrderFunction(Mycallbackfunction);
+// };
+// let MyHigherOrderFunction = (callback) =>{
+//     console.log("This is my higher order function");
+//     callback();
+// };
+// MyHigherOrderFunction(Mycallbackfunction);
 
 
-//set interval
-let count = 0;
-function Mycallbackfunction1(){
-    if (count === 5){
+// //set interval
+// let count = 0;
+// function Mycallbackfunction1(){
+//     if (count === 5){
+//     clearInterval(timer);
+//     console.log("CLEAR INTERVAL");
+//     return;
+//     }
+//     count++;
+//     console.log("This is number Count ",count);
+// }
+// let timer = setInterval(Mycallbackfunction1,1000);
+
+// // set timeout
+// let myCall = () => console.log("myCall executed");
+// setTimeout(myCall, 3000);
+
+// Example of setInterval and clearInterval
+let counter = 0;
+
+function increamentCounter() {
+  counter++;
+  console.log(counter);
+  if (counter === 5) {
     clearInterval(timer);
-    console.log("CLEAR INTERVAL");
-    return;
-    }
-    count++;
-    console.log("This is number Count ",count);
+    console.log("Timer Stopped");
+  }
 }
-let timer = setInterval(Mycallbackfunction1,1000);
+
+let timer = setInterval(increamentCounter, 1000);
