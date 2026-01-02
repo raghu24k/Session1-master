cbc1ca088d1cb08ef01d4d6a51599648756670cc
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-02 08:34:07 +0530

    put

--- batch2/promis.js
+++ batch2/promis.js
@@ -1,28 +1,10 @@
-let myCustompromise = new Promise ((resolve,reject)=>{
-    console.log("Resolve promise")
-    resolve([
-        {name: "abc", age: 23},
-        {name: "def", age: 25},
-        {name: "xyz", age: 21}
-    ]);
-});
+// callback funtion
+let Mycallbackfunction = () =>{
+    console.log("This is my callback function");
 
-
-let myCustompromise2 = new Promise ((resolve,reject)=>{
-    reject("failed");
-});
-
-
-async function fetchData(){
-    try{
-        let response = await myCustompromise;
-try{
-    let response = myCustompromise;
-    // console.log(response);
-    alert("1");
-    alert("2");
-    alert("3");
-}catch(e){
-    console.log("inside catch block");
-    console.log(e);
-}
+};
+let MyHigherOrderFunction = (callback) =>{
+    console.log("This is my higher order function");
+    callback();
+};
+MyHigherOrderFunction(Mycallbackfunction);
