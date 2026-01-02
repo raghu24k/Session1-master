8cfbc2c2b3a605a44a026e87c96de4a7bfa2b2ad
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-02 09:59:07 +0530

    post method

--- batch2/promis.js
+++ batch2/promis.js
@@ -82,10 +82,87 @@
 // promiseRejectCall();
 
 
-async function myFirstGetMethodDemo() {
-  let response = await fetch("http://gorest.co.in/public/v2/users");
-  console.log(response);
-}
-myFirstGetMethodDemo();
+// async function myFirstGetMethodDemo() {
+//   let response = await fetch("http://gorest.co.in/public/v2/users");
+//   console.log(response);
+// }
+
+
+//
+// async function myFirstGetMethodDemo() {
+//   try {
+//     let response = await fetch("http://gorest.co.in/public/v2/users");
+//     response = await response.json();
+//     console.log(response);
+//   } catch (error) {
+//     console.log("The error is:", error);
+//   }
+// }
+// myFirstGetMethodDemo();
 // let response = fetch("http://gorest.co.in/public/v2/users");
 // console.log(response);
+
+
+
+// async function Myfirstpostmethoddemo() {
+//   try{
+    
+//     let data ={
+//       name: "Raghuwinder Singh",
+//       email: "testuser@example.com",
+//       gender: "male",
+//       status: "active"
+//     };
+//     let options = {
+//       method: "POST",
+//       Headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//             Authorization: "cb4b4466877b3631baa723c2f483233e8ba5f3b23748bb213c823a06c16f5153"
+//         },
+//       body: JSON.stringify(data),
+//     };
+//     let response = await fetch("http://gorest.co.in/public/v2/users", options);
+//     response = await response.json();
+//     console.log(response);
+//   } catch (error){
+//     console.log("The error is:", error);
+//   }
+  
+// }
+// Myfirstpostmethoddemo();
+
+
+
+
+
+async function myFirstGetmethodDemo() {
+  try {
+    // access_token = "cb4b4466877b3631baa723c2f483233e8ba5f3b23748bb213c823a06c16f5153";
+    let data = {
+      name: "kumarer Thaanand",
+      gender: "male",
+      email: "r23k@gmail.com",
+      status: "Active",
+    };
+
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization:
+          "Bearer cb4b4466877b3631baa723c2f483233e8ba5f3b23748bb213c823a06c16f5153",
+      },
+      body: JSON.stringify(data),
+    };
+
+    let response = await fetch("https://gorest.co.in/public/v2/users", options);
+    response = await response.json();
+    console.log(response);
+  } catch (error) {
+    console.log("From Catch Block", error);
+  }
+}
+
+myFirstGetmethodDemo();
